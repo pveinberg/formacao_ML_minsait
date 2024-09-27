bd1ec0b3df8667940ae2cde699fc017c58fb102a
Pablo Veinberg <pveinberg@gmail.com>
2024-09-27 16:05:32 -0300

    Update and organize code

--- Notebooks/Aula01/preprocess.py
+++ Notebooks/Aula01/preprocess.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class Preprocess:
 
-    datasource_path = str
+    datasource_path: str = ...
     data = pd.DataFrame()
 
     def __init__(self, datasource_path) -> None:
@@ -21,7 +21,7 @@ class Preprocess:
         except Exception as e:
             raise e
 
-    def clean(self, cols_to_delete:list) -> None:
+    def clean(self, cols_to_delete: list) -> None:
         data = self.data
         # manual delete: when fields has not relevant
         if len(cols_to_delete) > 0:
@@ -39,25 +39,25 @@ class Preprocess:
         else:
             print("Nothing to do.")
 
-
         # missing values
         mv = data.isna().sum().sum()
         if mv > 0:
             for col in data.columns:
-                if data[col].isna().sum() > 0 and data[col].dtypes != 'object':
+                if data[col].isna().sum() > 0 and data[col].dtypes != "object":
                     col_median = data[col].median()
                     data[col].fillna(col_median, inplace=True)
-                elif data[col].isna().sum() > 0 and data[col].dtypes == 'object':
+                elif data[col].isna().sum() > 0 and data[col].dtypes == "object":
                     col_mode = data[col].mode()
                     data[col].fillna(col_mode, inplace=True)
         else:
             print("No NaN values in dataframe")
 
-        assert(data.isna().sum().sum()) == 0, "MISSING VALUES PERSISIT AFTER PREPROCESS"
-
+        assert (
+            data.isna().sum().sum()
+        ) == 0, "MISSING VALUES PERSISIT AFTER PREPROCESS"
 
         # integrity
-        
+
         # validation
 
         self.data = data
